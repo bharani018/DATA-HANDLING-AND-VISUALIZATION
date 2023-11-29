0e721694961fbfe43110c91bb8a46a153f29d5bb
bharani018 <114207030+bharani018@users.noreply.github.com>
2023-11-29 13:58:09 +0530

    Create 2.Title_and_color_to_pie_chart.r

---
+++ 2.Title_and_color_to_pie_chart.r
@@ -0,0 +1,7 @@
+# Creating data for the graph.  
+x <- c(20, 65, 15, 50)  
+labels <- c("India", "America", "Sri Lanka", "Nepal")  
+
+# Plotting the chart.  
+pie(x,labels,main="Country Pie chart",col=rainbow(length(x)))  
+ 
